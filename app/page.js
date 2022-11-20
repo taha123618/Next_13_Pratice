b4100a77ea0e86204bd96d965042dcf20608225e
taha123618 <tahaahmedanees2@gmail.com>
2022-11-20 23:34:36 +0500

    git action

--- app/page.js
+++ app/page.js
@@ -1,17 +1,19 @@
 import Link from "next/link";
-import React from "react";
+import React, { Suspense } from "react";
 import Image from "next/image";
 import show from "../public/show.jpeg";
 
 function page() {
   return (
     <>
-      <h1> Next 13</h1>
-      <Image src={show} width={500} height={500} alt="Images" />
-      <br />
-      <Link href="/posts">
-        <button>Posts</button>
-      </Link>
+      <Suspense fallback={<h1> Loading the Home Page................</h1>}>
+        <h1> Next 13</h1>
+        <Image src={show} width={500} height={500} alt="Images" />
+        <br />
+        <Link href="/posts">
+          <button>Posts</button>
+        </Link>
+      </Suspense>
     </>
   );
 }
